016a79af9232db487c822777547f723f5f7a6fa5
Iñigo Maruri <im818@ic.ac.uk>
2021-05-22 20:23:13 +0100

    Method that returns the largest integer in a list

---
+++ java_challenges/Maximum.java
@@ -0,0 +1,55 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class Maximum {
+  public static void main(String[] args) {
+      Integer[] numList = {1, 2, 3, 12, 4, 5};
+      Integer max = maximum(numList);
+      printNumberInWord(6);
+  }
+
+  public static Integer maximum(Integer[] numList) {
+      Integer currentMax = 0;
+      for(int i = 0; i < numList.length; i++){
+          if(numList[i] > currentMax){
+		currentMax = numList[i];
+	  }
+      }
+      return currentMax;
+  }
+
+  public static void printNumberInWord(int num){
+	switch(num){
+	        case 1:
+			System.out.println("ONE");
+			break;
+		case 2:
+			System.out.println("TWO");
+		        break;
+		case 3:
+ 			System.out.println("THREE");
+   		        break;
+	        case 4:
+	                System.out.println("FOUR");
+		        break;
+		case 5:
+		        System.out.println("FIVE");
+		        break;
+	        case 6:
+		        System.out.println("SIX");
+       		        break;
+                case 7:
+		        System.out.println("SEVEN");
+		        break;
+	        case 8:
+		        System.out.println("EIGHT");
+		        break;
+	        case 9:
+		        System.out.println("THREE");
+		        break;
+       	        default:
+		        System.out.println(">10");
+	}
+  }
+}
